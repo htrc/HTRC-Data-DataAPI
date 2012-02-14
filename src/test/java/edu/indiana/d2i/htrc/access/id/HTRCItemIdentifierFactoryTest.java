71ca0bb5a01784ba986d69e959b477fdf9c82326
Yiming Sun <yimsun@umail.iu.edu>
2012-02-14 01:21:36 +0000

    Added more test cases

--- src/test/java/edu/indiana/d2i/htrc/access/id/HTRCItemIdentifierFactoryTest.java
+++ src/test/java/edu/indiana/d2i/htrc/access/id/HTRCItemIdentifierFactoryTest.java
@@ -31,10 +31,14 @@
  */
 package edu.indiana.d2i.htrc.access.id;
 
-import junit.framework.Assert;
+import java.text.ParseException;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import edu.indiana.d2i.htrc.access.HTRCItemIdentifier;
+import edu.indiana.d2i.htrc.access.id.HTRCItemIdentifierFactory.IDTypeEnum;
 import edu.indiana.d2i.htrc.access.id.HTRCItemIdentifierFactory.Parser;
 
 /**
@@ -58,5 +62,54 @@ public class HTRCItemIdentifierFactoryTest {
         int pageSeq = 0;
         String pageSequence = Parser.generatePageSequenceString(pageSeq);
     }
+    
+    @Test
+    public void testVolumeIDsParserParse1() throws ParseException {
+        String rawString = "loc.ark:/13960/t9q23z43f |miun.ajj3079.0001.001";
+        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID);
+        String[] expected = new String[] {"loc.ark:/13960/t9q23z43f", "miun.ajj3079.0001.001"};
+        String[] actual = new String[2];
+
+        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
+        actual[0] = list.get(0).getVolumeID();
+        actual[1] = list.get(1).getVolumeID();
+            
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void testVolumeIDsParserParse2() throws ParseException {
+        String rawString = "loc.ark:/13960/t9q23z43f";
+        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID);
+        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
+        String actual = list.get(0).getVolumeID();
+        String expected = "loc.ark:/13960/t9q23z43f";
+        
+        Assert.assertEquals(expected, actual);
+    }
+    
+
+    @Test
+    public void testVolumeIDsParserParse3() throws ParseException {
+        String rawString = "loc.ark:/13960/t9q23z43f ||  | miun.ajj3079.0001.001";
+        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID);
+        String[] expected = new String[] {"loc.ark:/13960/t9q23z43f", "miun.ajj3079.0001.001"};
+        String[] actual = new String[2];
+
+        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
+        actual[0] = list.get(0).getVolumeID();
+        actual[1] = list.get(1).getVolumeID();
+            
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test(expected = ParseException.class)
+    public void testVolumeIDsParserParseError() throws ParseException {
+        String rawString = " ||  |";
+        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID);
+
+        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
+    }
+
 }
 
