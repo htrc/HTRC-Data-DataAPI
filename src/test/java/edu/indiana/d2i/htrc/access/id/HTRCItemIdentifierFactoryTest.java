c24fe496c92e30d8c7e3528636354a11ae26e2db
Yiming Sun <yimsun@umail.iu.edu>
2012-03-06 19:04:00 +0000

    added comments to each test case
    removed some cases that test policy violation checks
    
    modified some cases to retain the original goal but without using policy violation checks

--- src/test/java/edu/indiana/d2i/htrc/access/id/HTRCItemIdentifierFactoryTest.java
+++ src/test/java/edu/indiana/d2i/htrc/access/id/HTRCItemIdentifierFactoryTest.java
@@ -43,8 +43,6 @@ import edu.indiana.d2i.htrc.access.TestParameterContainer;
 import edu.indiana.d2i.htrc.access.exception.PolicyViolationException;
 import edu.indiana.d2i.htrc.access.id.HTRCItemIdentifierFactory.IDTypeEnum;
 import edu.indiana.d2i.htrc.access.id.HTRCItemIdentifierFactory.Parser;
-import edu.indiana.d2i.htrc.access.policy.MaxPagesPerVolumePolicyChecker;
-import edu.indiana.d2i.htrc.access.policy.MaxTotalPagesPolicyChecker;
 import edu.indiana.d2i.htrc.access.policy.MaxVolumesPolicyChecker;
 import edu.indiana.d2i.htrc.access.policy.NullPolicyCheckerRegistry;
 import edu.indiana.d2i.htrc.access.policy.PolicyCheckerRegistryImpl;
@@ -55,6 +53,7 @@ import edu.indiana.d2i.htrc.access.policy.PolicyCheckerRegistryImpl;
  */
 public class HTRCItemIdentifierFactoryTest {
     
+    // This case tests that a positive page number generates the correct page sequence string
     @Test
     public void testGeneratePageSequenceString() {
         int pageSeq = 1;
@@ -65,12 +64,14 @@ public class HTRCItemIdentifierFactoryTest {
         Assert.assertEquals(expectedPageSequence, pageSequence);
     }
     
+    // This case tests that attempts to generate page sequence string from a non-positive page number should raise an IllegalArgumentException 
     @Test(expected = IllegalArgumentException.class)
     public void testGeneratePageSequenceStringError() {
         int pageSeq = 0;
         String pageSequence = Parser.generatePageSequenceString(pageSeq);
     }
     
+    // This case tests that volumeIDs separated by a pipe should be correctly parsed
     @Test
     public void testVolumeIDsParserParse1() throws ParseException, PolicyViolationException {
         String rawString = "loc.ark:/13960/t9q23z43f |miun.ajj3079.0001.001";
@@ -85,6 +86,7 @@ public class HTRCItemIdentifierFactoryTest {
         Assert.assertArrayEquals(expected, actual);
     }
     
+    // This case tests that a single volumeID without pipe should be correctly parsed
     @Test
     public void testVolumeIDsParserParse2() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f";
@@ -96,7 +98,7 @@ public class HTRCItemIdentifierFactoryTest {
         Assert.assertEquals(expected, actual);
     }
     
-
+    // This cases tests that volumeIDs separated by multiple pipes and spaces should be tolerated and correctly parsed
     @Test
     public void testVolumeIDsParserParse3() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f ||  | miun.ajj3079.0001.001";
@@ -111,27 +113,7 @@ public class HTRCItemIdentifierFactoryTest {
         Assert.assertArrayEquals(expected, actual);
     }
     
-    @Test
-    public void testVolumeIDsParserParse4() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxVolumesPolicyChecker.PN_MAX_VOLUMES_ALLOWED, "2");
-//        MaxVolumesPolicyChecker.init(container);
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-        registry.registerPolicyChecker(MaxVolumesPolicyChecker.POLICY_NAME, new MaxVolumesPolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f |miun.ajj3079.0001.001";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID, registry);
-        String[] expected = new String[] {"loc.ark:/13960/t9q23z43f", "miun.ajj3079.0001.001"};
-        String[] actual = new String[2];
-
-        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
-        actual[0] = list.get(0).getVolumeID();
-        actual[1] = list.get(1).getVolumeID();
-            
-        Assert.assertArrayEquals(expected, actual);
-    }
-
-    
+    // This case tests that redundant volumeIDs in a request should be parsed and coalesced into only one occurance 
     @Test
     public void testVolumeIDsParserParse5() throws ParseException, PolicyViolationException {
         ParameterContainer container = new TestParameterContainer();
@@ -152,7 +134,8 @@ public class HTRCItemIdentifierFactoryTest {
         Assert.assertArrayEquals(expected, actual);
     }
 
-    
+   
+    // This case tests that at least one volumeID should be present or else the parser shall raise a ParseException
     @Test(expected = ParseException.class)
     public void testVolumeIDsParserParseError1() throws ParseException, PolicyViolationException  {
         String rawString = " ||  |";
@@ -161,22 +144,8 @@ public class HTRCItemIdentifierFactoryTest {
         List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
     }
     
-    @Test(expected = PolicyViolationException.class)
-    public void testVolumeIDsParserParseError2() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxVolumesPolicyChecker.PN_MAX_VOLUMES_ALLOWED, "2");
-//        MaxVolumesPolicyChecker.init(container);
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-        registry.registerPolicyChecker(MaxVolumesPolicyChecker.POLICY_NAME, new MaxVolumesPolicyChecker(container));
-        
-        String rawString = "loc.ark:/13960/t9q23z43f |  miun.ajj3079.0001.001 | loc.ark:/23456/909192";
-        
-        Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.VOLUME_ID, registry);
-        
-        parser.parse(rawString);
-    }
-    
 
+    // This case tests that pageIDs separated by pipe should be properly parsed, and page sequences can be multiple separated by comma, or can be a single pageSequence without comma
     @Test
     public void testPageIDsParserParse1() throws ParseException, PolicyViolationException {
         String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100>|  miun.ajj3079.0001.001<4>";
@@ -204,6 +173,7 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
     
+    // This case tests that multiple pipes and spaces can be tolerated and ignored by the parser as long as there is at least one valid pageID 
     @Test
     public void testPageIDsParserParse2() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100> | ||  ";
@@ -227,6 +197,7 @@ public class HTRCItemIdentifierFactoryTest {
     }
     
 
+    // This case tests that extra spaces in page sequence list can be tolerated and page sequences are correctly parsed
     @Test
     public void testPageIDsParserParse3() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f <1 ,2  , 44  , 100 >  ";
@@ -249,6 +220,7 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
 
+    // This case tests that multiple commas in page sequence list can be tolerated and page sequences are correctly parsed
     @Test
     public void testPageIDsParserParse4() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f < , , , 1, ,, 2,,, ,  44,,, 100,, ,, >  ";
@@ -271,70 +243,13 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
 
-    @Test
-    public void testPageIDsParserParse5() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxVolumesPolicyChecker.PN_MAX_VOLUMES_ALLOWED, "0");
-        container.setParameter(MaxTotalPagesPolicyChecker.PN_MAX_TOTAL_PAGES_ALLOWED, "0");
-        container.setParameter(MaxPagesPerVolumePolicyChecker.PN_MAX_PAGES_PER_VOLUME_ALLOWED, "0");
-        
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-
-//        MaxVolumesPolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxVolumesPolicyChecker.POLICY_NAME, new MaxVolumesPolicyChecker(container));
-        
-//        MaxTotalPagesPolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxTotalPagesPolicyChecker.POLICY_NAME, new MaxTotalPagesPolicyChecker(container));
-        
-//        MaxPagesPerVolumePolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxPagesPerVolumePolicyChecker.POLICY_NAME, new MaxPagesPerVolumePolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100>|  miun.ajj3079.0001.001<4>";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, registry);
-
-        String[] expectedVolIDs = new String[] {"loc.ark:/13960/t9q23z43f", "miun.ajj3079.0001.001"};
-        String[] actualVolIDs = new String[2];
-
-        List<? extends HTRCItemIdentifier> list = parser.parse(rawString);
-        
-        actualVolIDs[0] = list.get(0).getVolumeID();
-        actualVolIDs[1] = list.get(1).getVolumeID();
-        
-        Assert.assertArrayEquals(expectedVolIDs, actualVolIDs);
-        
-        String[] expectedPageSequences1 = new String[]{"00000001", "00000002", "00000044", "00000100"};
-        String[] actualPageSequences1 = list.get(0).getPageSequences().toArray(new String[0]);
-        
-        Assert.assertArrayEquals(actualPageSequences1.toString(), expectedPageSequences1, actualPageSequences1);
-        
-        String[] expectedPageSequences2 = new String[]{"00000004"};
-        String[] actualPageSequences2 = list.get(1).getPageSequences().toArray(new String[0]);
-        
-        Assert.assertArrayEquals(actualPageSequences2.toString(), expectedPageSequences2, actualPageSequences2);
-        
-    }
-
     
+    // This case tests that redundant volume identifiers in pageIDs should be coalesced and redundant page sequences should be coalesced by the parser
     @Test
     public void testPageIDsParserParse6() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxVolumesPolicyChecker.PN_MAX_VOLUMES_ALLOWED, "2");
-        container.setParameter(MaxTotalPagesPolicyChecker.PN_MAX_TOTAL_PAGES_ALLOWED, "10");
-        container.setParameter(MaxPagesPerVolumePolicyChecker.PN_MAX_PAGES_PER_VOLUME_ALLOWED, "8");
-        
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
 
-//        MaxVolumesPolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxVolumesPolicyChecker.POLICY_NAME, new MaxVolumesPolicyChecker(container));
-        
-//        MaxTotalPagesPolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxTotalPagesPolicyChecker.POLICY_NAME, new MaxTotalPagesPolicyChecker(container));
-        
-//        MaxPagesPerVolumePolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxPagesPerVolumePolicyChecker.POLICY_NAME, new MaxPagesPerVolumePolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100>|  miun.ajj3079.0001.001<4> | loc.ark:/13960/t9q23z43f <22, 12, 17>";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, registry);
+        String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100, 2, 44>|  miun.ajj3079.0001.001<4> | loc.ark:/13960/t9q23z43f <22, 12, 2, 17>";
+        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, new NullPolicyCheckerRegistry());
 
         String[] expectedVolIDs = new String[] {"loc.ark:/13960/t9q23z43f", "miun.ajj3079.0001.001"};
         String[] actualVolIDs = new String[2];
@@ -358,7 +273,8 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
     
-    
+
+    // This case tests that a ParseException should be raised if a page sequence is not positive
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError1() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f < 1, -1 >  ";
@@ -368,6 +284,7 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
 
+    // This case tests that a ParseException should be raised if a page sequence is not numeric
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError2() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f < one, two >  ";
@@ -378,6 +295,7 @@ public class HTRCItemIdentifierFactoryTest {
     }
 
 
+    // This case tests that a ParseException should be raised if page sequence list is malformed (missing closing angle bracket)
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError3() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f <  ";
@@ -388,6 +306,7 @@ public class HTRCItemIdentifierFactoryTest {
     }
 
 
+    // This case tests that a ParseException should be raised if page sequence list is malformed (missing opening angle bracket)
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError4() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f 3,4,8,10>  ";
@@ -397,6 +316,7 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
 
+    // This case tests that a ParseException should be raised if the page sequence list does not have page sequences
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError5() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f<>";
@@ -406,6 +326,7 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
     
+    // This case tests that a ParseException should be raised if the pageID does not have the volume identifier portion
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError6() throws ParseException, PolicyViolationException  {
         String rawString = "<1,2,3,4>";
@@ -416,6 +337,7 @@ public class HTRCItemIdentifierFactoryTest {
     }
 
     
+    // This case tests that a ParseException should be raised if the page ID does not have the page sequence list portion
     @Test (expected = ParseException.class)
     public void testPageIDsParserParseError7() throws ParseException, PolicyViolationException  {
         String rawString = "loc.ark:/13960/t9q23z43f";
@@ -425,67 +347,6 @@ public class HTRCItemIdentifierFactoryTest {
         
     }
     
-    @Test (expected = PolicyViolationException.class)
-    public void testPageIDsParserParseError8() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxTotalPagesPolicyChecker.PN_MAX_TOTAL_PAGES_ALLOWED, "4");
-        container.setParameter(MaxPagesPerVolumePolicyChecker.PN_MAX_PAGES_PER_VOLUME_ALLOWED, "4");
-
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-
-//        MaxTotalPagesPolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxTotalPagesPolicyChecker.POLICY_NAME, new MaxTotalPagesPolicyChecker(container));
-        
-//        MaxPagesPerVolumePolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxPagesPerVolumePolicyChecker.POLICY_NAME, new MaxPagesPerVolumePolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100>|  miun.ajj3079.0001.001<4>";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, registry);
-
-        parser.parse(rawString);
-    }
-
-
-    @Test (expected = PolicyViolationException.class)
-    public void testPageIDsParserParseError9() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxTotalPagesPolicyChecker.PN_MAX_TOTAL_PAGES_ALLOWED, "100");
-        container.setParameter(MaxPagesPerVolumePolicyChecker.PN_MAX_PAGES_PER_VOLUME_ALLOWED, "3");
-        
-//        MaxTotalPagesPolicyChecker.init(container);
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-        registry.registerPolicyChecker(MaxTotalPagesPolicyChecker.POLICY_NAME, new MaxTotalPagesPolicyChecker(container));
-        
-//        MaxPagesPerVolumePolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxPagesPerVolumePolicyChecker.POLICY_NAME, new MaxPagesPerVolumePolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f <1,2, 44, 100>|  miun.ajj3079.0001.001<4>";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, registry);
-
-        parser.parse(rawString);
-    }
-
-
-
-    @Test (expected = PolicyViolationException.class)
-    public void testPageIDsParserParseError10() throws ParseException, PolicyViolationException {
-        ParameterContainer container = new TestParameterContainer();
-        container.setParameter(MaxTotalPagesPolicyChecker.PN_MAX_TOTAL_PAGES_ALLOWED, "100");
-        container.setParameter(MaxPagesPerVolumePolicyChecker.PN_MAX_PAGES_PER_VOLUME_ALLOWED, "3");
-        
-//        MaxTotalPagesPolicyChecker.init(container);
-        PolicyCheckerRegistryImpl registry = PolicyCheckerRegistryImpl.getInstance();
-        registry.registerPolicyChecker(MaxTotalPagesPolicyChecker.POLICY_NAME, new MaxTotalPagesPolicyChecker(container));
-        
-//        MaxPagesPerVolumePolicyChecker.init(container);
-        registry.registerPolicyChecker(MaxPagesPerVolumePolicyChecker.POLICY_NAME, new MaxPagesPerVolumePolicyChecker(container));
-
-        String rawString = "loc.ark:/13960/t9q23z43f <1> |loc.ark:/13960/t9q23z43f<2>|loc.ark:/13960/t9q23z43f<44>|loc.ark:/13960/t9q23z43f<100>";
-        HTRCItemIdentifierFactory.Parser parser = HTRCItemIdentifierFactory.getParser(IDTypeEnum.PAGE_ID, registry);
-
-        parser.parse(rawString);
-    }
-
 
 }
 
